70ed9032b62b22c3391e79b61bdd0946073c7a3f
FELIPE RAMIREZ LOAIZA <feliperamirez298927@correo.itm.edu.co>
2023-04-15 21:29:18 -0500

    New WebAPI Shopping project with new Country Entity

---
+++ ShoppingWebAPI/ShoppingWebAPI/Entities/Entity.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ShoppingWebAPI.Entities
+{
+    public class Entity
+    {
+        [Key] //LLAVE PRIMARIA
+        public Guid Id { get; set; }
+
+        public DateTime? CreatedDate { get; set; }
+        public DateTime? ModifiedDate { get; set; }
+    }
+}
